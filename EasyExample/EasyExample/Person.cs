0a1237ce378ce49ca1ecc7197ac1d097dc5e6aaa
luisfsangil <luisfsangil@gmail.com>
2021-08-14 08:59:11 +0200

    Class Person

---
+++ EasyExample/EasyExample/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyExample
+{
+    class Person{
+        //Encapsulated atributes
+        private string name;
+        private byte age;
+        //Public Constructors
+        public Person(){
+            name = "";
+            age = 0;
+        }
+        public Person(string _name){
+            name = _name;
+            age = 0;
+        }
+        public Person(string _name, byte _age){
+            name = _name;
+            if (_age>=0)    age = _age;
+            else throw new Exception(); //The age of a person must be >=0
+        }
+        //Public Methods Guetters and Setters
+        //Guetters
+        public string GetName() => name;
+        public byte GetAge() => age;
+        //Setters
+        public void setName(string _name){
+            name = _name;
+        }
+        public void SetAge(byte _age){
+            if (_age >= 0) age = _age;
+            else throw new Exception(); //The age of a person must be >=0
+        }
+    }
+}
---
+++ EasyExample/EasyExample/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//The machine start's the compilation and interpretation here
+using EasyExample;
+//Using the namespace 'EasyExample'
+
+Person luis = new Person();
+//Object 'luis' of class 'Person' instantiate and initialited
+
