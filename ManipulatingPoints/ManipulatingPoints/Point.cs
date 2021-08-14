7cbabb813aacce4ce2721bc53548cd29346986a8
luisfsangil <luisfsangil@gmail.com>
2021-08-14 09:59:34 +0200

    Manipulating Points

---
+++ ManipulatingPoints/ManipulatingPoints/Point.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulatingPoints
+{
+    class Point{
+        //Encapsulated atributtes of the class
+        private float x, y;
+        //Public constructors
+
+        //The default constructor initialize the point in the origin (0,0)
+        public Point(){
+            x=y=0;
+        }
+        //This constructor have the coordenade y like a optional parameter
+        //If you dont set a y value, the value is cero
+        public Point(float x,float y=0){
+            this.x=x;
+            this.y=y;
+        }
+        //Public Methods
+        //Guetters
+        public float getX() => x;
+        public float getY() => y;
+        //Setters
+        public void setX(float _x){
+            this.x = _x;
+        }
+        public void setY(float _y){
+            this.y = _y;
+        }
+        public void setPoint(float _x, float _y){
+            this.x = _x;
+            this.y = _y;
+        }
+        //Other Methods
+        //Methods for increment the coordenades
+        public void incrementX(float _inc){
+            this.x += _inc;
+        }
+        public void incrementY(float _inc){
+            this.y += _inc;
+        }
+        public void incrementXY(float _incx,float _incy){
+            this.x+=_incx;
+            this.y+=_incy;
+        }
+        //Methods for decrement the coordenades
+        public void decrementX(float _inc)
+        {
+            this.x -= _inc;
+        }
+        public void decrementY(float _inc)
+        {
+            this.y -= _inc;
+        }
+        public void decrementXY(float _incx, float _incy)
+        {
+            this.x -= _incx;
+            this.y -= _incy;
+        }
+    }
+}
---
+++ ManipulatingPoints/ManipulatingPoints/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//The code start's here
+
+using ManipulatingPoints;
+
+Point origin = new Point(); //The origin of coordenades
+Point onePoint = new Point(50); //onePoint is in the (50,0) coordenades
+Point otherPoint = new Point(20,180);   //otherPoint is in the (20,180) coordenades
+
+onePoint.incrementY(20);
+Console.WriteLine(onePoint.getY());
