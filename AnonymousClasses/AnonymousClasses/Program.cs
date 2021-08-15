bb108ebf49fd78fb3321b2e273af635717f3a964
luisfsangil <luisfsangil@gmail.com>
2021-08-15 10:29:40 +0200

    anonymous classes

---
+++ AnonymousClasses/AnonymousClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Here is an example of Anonymous classes.
+
+
+//If two or more objects have the same number of fields, with the same name, in the same order and with the same data type, both object belongs the same class
+var objectOne = new { Name = "Luis", Age = 27 };
+var objectTwo = new { Name = "Marta", Age = 32 };
+objectOne = objectTwo;
+
+//There are a few rules with the anonymous classes:
+    //All the fields are public
+    //All the fields have to been initialiced
+    //The fields can't be static
+    //This clases don't have methods
